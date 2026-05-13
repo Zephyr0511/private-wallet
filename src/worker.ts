4f201758d50d0d05edde46ba2e7a3e2b8d99b4f3
枕枝鹤 <2305459046@qq.com>
2026-05-13 12:43:08 +0800

    fix worker asset binding type

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,9 @@
+type AssetBinding = {
+  fetch(input: Request | string | URL, init?: RequestInit): Promise<Response>
+}
+
 export default {
-  async fetch(request: Request, env: { ASSETS: Fetcher }) {
+  async fetch(request: Request, env: { ASSETS: AssetBinding }) {
     return env.ASSETS.fetch(request)
   },
 }
